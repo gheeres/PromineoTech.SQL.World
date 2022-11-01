65a4d2c51cbff02701ac31c0436d2d58d511f438
George Heeres <gheeres@gmail.com>
2022-11-01 18:43:45 -0500

    Week9: Start

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module myworld {
+  exports world;
+  exports world.exception;
+  //exports world.dao;
+  //exports world.entity;
+  //exports world.service;
+
+  requires java.sql;
+}
---
+++ src/main/java/world/Application.java
@@ -0,0 +1,21 @@
+package world;
+
+public class Application {
+  public Application() {
+  }
+  
+  /**
+   * The instance run/entry point.
+   * @param args The optional command line arguments.
+   */
+  public void run(String[] args) {
+  }
+  
+  /**
+   * Standard / main entry point for application.
+   * @param args
+   */
+  public static void main(String[] args) {
+    new Application().run(args);
+  }
+}
