44c094762087a13b67517238d87fa4cac0b2651d
George Heeres <gheeres@gmail.com>
2023-04-18 19:40:42 -0500

    initial project / skeleton

---
+++ src/main/java/world/Application.java
@@ -0,0 +1,24 @@
+package world;
+
+import java.util.Scanner;
+
+public class Application {
+  /**
+   * Standard / main entry point for application.
+   * @param args
+   */
+  public static void main(String[] args) {
+    new Application().run(args);
+  }
+
+  public Application() {
+  }
+  
+  /**
+   * The instance run/entry point.
+   * @param args The optional command line arguments.
+   */
+  public void run(String[] args) {
+    Scanner input = new Scanner(System.in);
+  }
+}
